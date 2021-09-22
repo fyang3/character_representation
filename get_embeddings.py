11f8f124111fd8af0f3e4cb24cdbeb179929c1a9
fyang3 <42852118+fyang3@users.noreply.github.com>
2021-09-21 20:57:47 -0400

    first pass of embeddings retrival pipeline with modularized results

--- get_embeddings.py
+++ get_embeddings.py
@@ -1,4 +1,4 @@
-from transformers import BertModel,BertTokenizer,BertForMaskedLM
+from transformers import BertModel,BertTokenizer,BertForMaskedLM, AutoTokenizer, AutoModel
 import time
 import numpy as np
 import math
@@ -35,44 +35,85 @@ def main():
     
     print("Using model:",model_name,file=sys.stderr)
     
-    trainf = sys.argv[3]
-    traintype = sys.argv[4]
-    save_path = sys.argv[5]
-    # datasize = sys.argv[6]
-    mask_lookup = sys.argv[7]=="mask"
+    trainf = sys.argv[2]
+    #save_path = sys.argv[3] temporarily disable
     bert = models[model].from_pretrained(model_name)
     bert.to(my_device)
     print("Retreiving word embeddings for training file: ", trainf)
-    tr_full_data,tr_emb_data,tr_labels = get_data(trainf,traintype)
-    tr_size = len(tr_full_data)
-    outlines = []
-    for l in range(len(tr_emb_data)):
-        forms = tr_full_data[l][-6:-1]
-        correct = tr_full_data[l][1]
-        if not mask_lookup:
-            tar_embed,_, _ = get_bert_word_embed(bert,tr_emb_data[l],correct)
-        mask_embed,scores = get_bert_preds(bert,tr_emb_data[l],forms)
-        outcomes = score_preds(scores,forms,correct)
-        if mask_lookup:
-            final_emb = mask_embed
-        else:
-            final_emb = tar_embed
-        nl = make_line(outcomes,final_emb)
-        outlines.append(nl)
-    with open(datasize+"embeds.csv",'w') as csvf:
-        csvwriter = csv.writer(csvf, delimiter='\t')
-        for l in outlines:
-            pl = '\t'.join(l)+'\n'
-            #print(pl)
-            csvwriter.writerow(l)
+    # tr_full_data,tr_emb_data,tr_labels = get_data(trainf,traintype)     # Funing: still need to understand how to adapt the get_data for data prep - seems not much prep is necessary
+    # tr_size = len(tr_full_data)
+    data = get_data(trainf)[1:] #strip headers
+    output = []
+    for entry in data:
+        embeds = get_embeddings_rev(entry[0], entry[1], layers=None)
+        output.append([entry[0],entry[1],embeds])
+    #with open('{file_path}.csv'.format(file_path=os.path.join(save_path, trainf+"embeds.csv"), 'w+',new_line="") as csv_file:
+    with open(trainf+"embeds.csv",'w') as csvf:
+        csvwriter = csv.writer(csvf)
+        csvwriter.writerows(output)
+
     return
 
 
 
+def get_data(corpus_name):
+    '''process csv file to a list of sentences for embedding retrieval'''
+    processed = []
+    with open(corpus_name,'r') as f:
+        csv_reader = csv.reader(f)
+        for row in csv_reader:
+            processed.append([row[0],row[1]])
+        return processed
+
+def get_word_idx(sent: str, word: str):
+     return sent.split(" ").index(word)
+ 
+ 
+def get_hidden_states(encoded, token_ids_word, model, layers):
+     """Push input IDs through model. Stack and sum `layers` (last four by default).
+        Select only those subword token outputs that belong to our word of interest
+        and average them."""
+     with torch.no_grad():
+        output = model(**encoded)
+ 
+     # Get all hidden states
+     states = output.hidden_states
+     # Stack and sum all requested layers
+     output = torch.stack([states[i] for i in layers]).sum(0).squeeze()
+     # Only select the tokens that constitute the requested word
+     word_tokens_output = output[token_ids_word]
+ 
+     return word_tokens_output.mean(dim=0)
+ 
+ 
+def get_word_vector(sent, idx, tokenizer, model, layers):
+     """Get a word vector by first tokenizing the input sentence, getting all token idxs
+        that make up the word of interest, and then `get_hidden_states`."""
+     encoded = tokenizer.encode_plus(sent, return_tensors="pt")
+     # get all token idxs that belong to the word of interest
+     token_ids_word = np.where(np.array(encoded.word_ids()) == idx)
+ 
+     return get_hidden_states(encoded, token_ids_word, model, layers)
+ 
+ 
+def get_embeddings_rev(sent, word, layers=None): #meta-function to retrive contextual embeddings for a given sentence
+     # Use last four layers by default
+     layers = [-4, -3, -2, -1] if not layers else layers
+     tokenizer = AutoTokenizer.from_pretrained("bert-base-cased") #should change to accomodate more models
+     model = AutoModel.from_pretrained("bert-base-cased", output_hidden_states=True)
+  
+     idx = get_word_idx(sent, word)
+     word_embedding = get_word_vector(sent, idx, tokenizer, model, layers)
+     
+     return word_embedding 
+ 
+ 
+if __name__ == '__main__':
+     main()
+    
 
 
-
-def get_bert_preds(bert,emb_line,forms,seq_length=0):
+def get_bert_preds(bert,emb_line,forms,seq_length=0): #from Carolyn's scripts, and modifying
     pre = emb_line[0]
     post = emb_line[1]
     if seq_length == 0:
